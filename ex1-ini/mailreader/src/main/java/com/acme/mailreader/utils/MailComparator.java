9a097f04ba8b1e1ddfab02c7ff469ae8c3990e8b
Quentin MALINGE <quentin.malinge@gmail.com>
2018-03-22 15:02:03 +0100

    Exo1-2

--- ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
+++ ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
@@ -10,29 +10,68 @@ import com.acme.mailreader.model.Mail;
  * Comme on désire afficher les mails les plus importants en premier, l'element le plus grand retourne une valeur négative
  *
  */
+
 public class MailComparator implements Comparator<Mail> {
 
-	public int compare(Mail obj1, Mail obj2) {
-		if (obj1 == null || obj2 == null) {
-			return 0;
-		}
-		if (obj1.isImportant() != obj2.isImportant()) {
-			if (obj1.isImportant() && !obj2.isImportant()) {
-				return -1;
-			} else {
-				return 1;
-			}
-		}
-		if (obj1.getStatut() != obj2.getStatut()) {
-			int comp = obj1.getStatut().ordinal()
-					- obj2.getStatut().ordinal();
-			return comp > 0 ? -1 : 1;
-		}
-		if (obj1.getSujet() != obj2.getSujet()) {
-			return obj2.getSujet().compareTo(obj1.getSujet());
-		}
-		return obj2.getDate().compareTo(obj1.getDate());
-	}
-	
+	public static final int EGAUX = 0;
+	public static final int PREMIER_PLUS_PETIT = 1;
+	public static final int PREMIER_PLUS_GRAND = -1;
 
+	public int compare(Mail mail1, Mail mail2) {
+		if (unDesMailsNul(mail1, mail2)) {
+			return EGAUX;
+		}
+		if (importanceDifferentsMails(mail1, mail2)) {
+			return trierParImportance(mail1, mail2);
+		}
+		if (statutDifferentsMails(mail1, mail2)) {
+			return trierParStatut(mail1, mail2);
+		}
+		if (sujetsDifferentsMails(mail1, mail2)) {
+			return trierParSujet(mail1, mail2);
+		}
+		return mail1.getDate().compareTo(mail2.getDate());
+	}
+
+	private int trierParImportance(Mail mail1, Mail mail2) {
+		if (mail1.isImportant() && !mail2.isImportant()) {
+			return PREMIER_PLUS_GRAND;
+		} else {
+			return PREMIER_PLUS_PETIT;
+		}
+	}
+
+	private int trierParStatut(Mail mail1, Mail mail2) {
+		int comp = mail1.getStatut().ordinal() - mail2.getStatut().ordinal();
+		return comp < 0 ? PREMIER_PLUS_PETIT : PREMIER_PLUS_GRAND;
+	}
+
+	private int trierParSujet(Mail mail1, Mail mail2) {
+		if (mail1.getSujet() == null && mail2.getSujet() != null) {
+			return PREMIER_PLUS_PETIT;
+		}
+		else if (mail1.getSujet() != null && mail2.getSujet() == null) {
+			return PREMIER_PLUS_GRAND;
+		}
+		return mail1.getSujet().compareTo(mail2.getSujet());
+	}
+
+	private boolean unDesMailsNul(Mail mail1, Mail mail2) {
+		return mail1 == null || mail2 == null;
+	}
+
+	private boolean importanceDifferentsMails(Mail mail1, Mail mail2) {
+		return mail1.isImportant() != mail2.isImportant();
+	}
+
+	private boolean statutDifferentsMails(Mail mail1, Mail mail2) {
+		return mail1.getStatut() != mail2.getStatut();
+	}
+
+	private boolean sujetsDifferentsMails(Mail mail1, Mail mail2) {
+		if (mail1.getSujet() == null || mail2.getSujet() == null) {
+			return true;
+		}
+		return !mail1.getSujet().equals(mail2.getSujet());
+	}
 }
